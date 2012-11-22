8ccba6bbfcd8a8598e7f1c8ec6c42756ed3bb256
Jimmy Huang <jimmy.huang@splashtop.com>
2012-11-22 19:06:53 +0800

    Fix bugs in invoice_adjustment, refund, to make it usable

--- lib/zuora/objects/refund_invoice_payment.rb
+++ lib/zuora/objects/refund_invoice_payment.rb
@@ -1,7 +1,7 @@
-#module Zuora::Objects
-  #class RefundInvoicePayment < Base
-    #belongs_to :invoice_payment
-    #belongs_to :refund
+module Zuora::Objects
+  class RefundInvoicePayment < Base
+    belongs_to :invoice_payment
+    belongs_to :refund
 
     #validates_presence_of :invoice_payment_id, :refund_amount, :refund_id
 
@@ -10,9 +10,9 @@
     #validates_length_of   :updated_by_id, :maximum => 32
     #validates_datetime_of :update_date, :allow_nil => true
 
-    #define_attributes do
-      #read_only :created_by_id, :created_date, :invoice_payment_id, :refund_amount,
-        #:refund_id, :updated_by_id, :updated_date
-    #end
-  #end
-#end
+    define_attributes do
+      read_only :created_by_id, :created_date, :invoice_payment_id, :refund_amount,
+        :refund_id, :updated_by_id, :updated_date
+    end
+  end
+end
